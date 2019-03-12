dbd1ee8de75e6e2587d94654f02de33b4685fdbf
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-03-12 18:07:04 -0400

    added a function for the radio buttons to change to selected image

--- app/src/main/java/com/example/images/MainActivity.kt
+++ app/src/main/java/com/example/images/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.images
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun radioButtonClick(view: View){
+        if(view == rb_eevee){
+            //telling to use image view to use the image
+        pokemon.setImageResource(R.drawable.Eevee)
+        }
+
+        if(view == rb_pikachu){
+            //telling to use image view to use the image
+            pokemon.setImageResource(R.drawable.Pikachu)
+        }
+        if(view == rb_rockruff){
+            //telling to use image view to use the image
+            pokemon.setImageResource(R.drawable.Rockruff)
+        }
     }
 }
